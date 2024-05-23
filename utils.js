b0ce1901e2597b29efa6caf1923308566e19c65c
mveraalbarenque <don.marcos.vera@gmail.com>
2024-05-22 21:49:38 -0400

    add function "resta"

--- utils.js
+++ utils.js
@@ -4,3 +4,10 @@ const sumar = (a, b) => {
   }
   return a + b;
 };
+
+const resta = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    alert("Debes ingresar numeros");
+  }
+  return a - b;
+};
